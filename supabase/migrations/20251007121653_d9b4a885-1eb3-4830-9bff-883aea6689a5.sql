8564de9a5dabd64b6eb4d15e4df8d03794a84aba
P.Sannith <88277591+Sannith-Hack@users.noreply.github.com>
2025-10-08 20:34:56 +0530

    Delete supabase directory

--- supabase/migrations/20251007121653_d9b4a885-1eb3-4830-9bff-883aea6689a5.sql
+++
@@ -1,5 +0,0 @@
--- Add DELETE policy for students table to allow admins to manage data retention
-CREATE POLICY "Admins can delete student records"
-ON public.students
-FOR DELETE
-USING (public.has_role(auth.uid(), 'admin'));
